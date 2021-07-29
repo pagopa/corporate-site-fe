6bdc91529597b5e0212acd920f1f36ab52acb9e3
Simone Notargiacomo <simone.notargiacomo@justbit.it>
2021-07-29 11:04:43 +0200

    Fix DevOps

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,11 +19,11 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         url: `${process.env.BACKEND_BASE_URL}/wp/index.php?graphql`,
-        // schema: {
-        //   perPage: 20, // default 100
-        //   requestConcurrency: 5, // default 15
-        //   previewRequestConcurrency: 2, // default 5
-        // }
+        schema: {
+          perPage: 20, // default 100
+          requestConcurrency: 2, // default 15
+          previewRequestConcurrency: 2, // default 5
+        }
       },
     },
     {
