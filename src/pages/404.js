077664ee6e3939fd945a44c803ba628948d283bc
justbitdev <dev@justbit.it>
2021-08-06 11:43:06 +0200

    locale fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,31 +5,34 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from '../partials/Layout'
 import SeoHelmet from '../components/SeoHelmet'
 
-const NotFoundPage = () => (
-  <Layout>
-    
-    <section className="block --block-text text --centered">
+const NotFoundPage = (props) => {
+  console.log()
+  return (
+    <Layout locale={props.location.pathname.split('/')[1]}>
+      
+      <section className="block --block-text text --centered">
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-            <h1>Ops! Non troviamo la<br />pagina che stai cercando.</h1>
-            <p>Sembra che la pagina che stai cercando non esista su questo sito. Prova a digitare correttamente l’URL, se non funziona è possibile che la pagina sia stata eliminata o che sia in fase di progettazione</p>
-            <div style={{
-              paddingLeft: '10%',
-              marginTop: '8rem',
-              marginBottom: '8rem'
-            }} >
-              <StaticImage src="../images/404.png" alt="image" width={600} />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+              <h1>Ops! Non troviamo la<br />pagina che stai cercando.</h1>
+              <p>Sembra che la pagina che stai cercando non esista su questo sito. Prova a digitare correttamente l’URL, se non funziona è possibile che la pagina sia stata eliminata o che sia in fase di progettazione</p>
+              <div style={{
+                paddingLeft: '10%',
+                marginTop: '8rem',
+                marginBottom: '8rem'
+              }} >
+                <StaticImage src="../images/404.png" alt="image" width={600} />
+              </div>
+                
             </div>
-              
           </div>
         </div>
-      </div>
 
-    </section>
+      </section>
 
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default NotFoundPage
