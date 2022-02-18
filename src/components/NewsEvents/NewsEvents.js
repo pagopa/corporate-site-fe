21064f5af12e6232a6862f742515e4e4d4d55e1c
justbitdev <dev@justbit.it>
2022-02-18 11:17:42 +0100

    changed news events component entries with latest and including initiatives

--- src/components/NewsEvents/NewsEvents.js
+++ src/components/NewsEvents/NewsEvents.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 
 import { LocaleContext } from '../../contexts/LocaleContext.js'
+
+import { useInitiatives } from '../../hooks/useInitiatives.js'
 import { useEvents } from '../../hooks/useEvents.js'
 import { useNews } from '../../hooks/useNews.js'
 
@@ -15,25 +17,35 @@ const LatestEntries = () => {
 
   const news = useNews()
   const events = useEvents()
+  const initiatives = useInitiatives()
 
   const currentLocaleNews = news.filter(j => j.node.locale.id === locale)
   const currentLocaleEvents = events.filter(j => j.node.locale.id === locale)
+  const currentLocaleInitiatives = initiatives.filter(
+    j => j.node.locale.id === locale
+  )
+
+  const collection = [
+    ...currentLocaleNews,
+    ...currentLocaleEvents,
+    ...currentLocaleInitiatives,
+  ]
+
+  const sortComparing = (a, b) => {
+    const aDate = new Date(a.node.date).getTime()
+    const bDate = new Date(b.node.date).getTime()
+    return bDate - aDate
+  }
+  
+  collection.sort(sortComparing)
+
 
   return (
     <>
-      {currentLocaleNews.map((pr, key) => {
-
+      {collection.slice(0,2).map((post, key) => {
         return (
-          <div className="col-12 col-lg-5 d-flex" key={key}>
-            <Post data={pr.node} />
-          </div>
-        )
-      })}
-
-      {currentLocaleEvents.map((pr, key) => {
-        return (
-          <div className={`col-12 col-lg-5${currentLocaleNews.length ? ' offset-lg-1' : ''} d-flex`} key={key}>
-            <Post data={pr.node} />
+          <div className={`col-12 col-lg-5 d-flex${key ? ' offset-lg-1' : ''}`} key={key}>
+            <Post data={post.node} />
           </div>
         )
       })}
