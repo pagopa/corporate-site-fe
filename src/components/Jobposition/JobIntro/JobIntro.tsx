2fadb5e5d4e072ab395b33a65bf2d439a64a9f45
Michele Moio <michele.moio@protonmail.com>
2023-02-03 11:46:28 +0100

    refactor: SharedBlockBody
     - SharedBlock->SharedBlockBody renaming
     - SharedBlockBody accept SharedBlockBodyFragment type props

--- src/components/Jobposition/JobIntro/JobIntro.tsx
+++ src/components/Jobposition/JobIntro/JobIntro.tsx
@@ -1,132 +1,132 @@
-import React from 'react';
 import { graphql } from 'gatsby';
-import { SharedBlock } from '../../SharedBlock';
+import React from 'react';
+import { SharedBlockBody } from '../../SharedBlockBody';
 
 const dateFormatOptions: Intl.DateTimeFormatOptions = {
-	year: 'numeric',
-	month: '2-digit',
-	day: '2-digit',
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
 };
 
 export const SharedBlockFragment = graphql`
-	fragment SharedBlock on STRAPI__COMPONENT_SHARED_BLOCK_INTRO {
-		eyelet
-		id
-		introMenu
-		isEventLanding
-		strapi_id
-		title
-		useFeaturedImage
-		body {
-			...SharedBlockBody
-		}
-	}
+  fragment SharedBlock on STRAPI__COMPONENT_SHARED_BLOCK_INTRO {
+    eyelet
+    id
+    introMenu
+    isEventLanding
+    strapi_id
+    title
+    useFeaturedImage
+    body {
+      ...SharedBlockBody
+    }
+  }
 `;
 
 export const JobIntroFragment = graphql`
-	fragment JobIntro on STRAPI_JOBPOSITION {
-		closeDate
-		hiredPositions
-		openDate
-		openPositions
-		selectedPeople
-		blocks {
-			...SharedBlock
-		}
-	}
+  fragment JobIntro on STRAPI_JOBPOSITION {
+    closeDate
+    hiredPositions
+    openDate
+    openPositions
+    selectedPeople
+    blocks {
+      ...SharedBlock
+    }
+  }
 `;
 
 export const JobIntro = ({
-	data: {
-		closeDate,
-		hiredPositions,
-		openDate,
-		openPositions,
-		selectedPeople,
-		blocks,
-	},
+  data: {
+    closeDate,
+    hiredPositions,
+    openDate,
+    openPositions,
+    selectedPeople,
+    blocks,
+  },
 }: {
-	data: Queries.JobIntroFragment;
+  data: Queries.JobIntroFragment;
 }) => {
-	const block: Queries.SharedBlockFragment | null = blocks ? blocks[0] : null;
+  const block: Queries.SharedBlockFragment | null = blocks ? blocks[0] : null;
 
-	const startDate = openDate
-		? new Date(openDate).toLocaleDateString('it', dateFormatOptions)
-		: null;
-	const endDate = closeDate
-		? new Date(closeDate).toLocaleDateString('it', dateFormatOptions)
-		: null;
+  const startDate = openDate
+    ? new Date(openDate).toLocaleDateString('it', dateFormatOptions)
+    : null;
+  const endDate = closeDate
+    ? new Date(closeDate).toLocaleDateString('it', dateFormatOptions)
+    : null;
 
-	const hasPositionsData = openPositions || hiredPositions ? true : false;
-	const hasSelectionData = selectedPeople ? true : false;
+  const hasPositionsData = openPositions || hiredPositions ? true : false;
+  const hasSelectionData = selectedPeople ? true : false;
 
-	return (
-		<header className="block --block-intro intro --job">
-			<div className="container-fluid">
-				{block && (
-					<div className="row">
-						<div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-							<div className="intro__heading">
-								{block.eyelet && <h4>{block.eyelet}</h4>}
-								{block.title && <h1>{block.title}</h1>}
-							</div>
-						</div>
-					</div>
-				)}
+  return (
+    <header className="block --block-intro intro --job">
+      <div className="container-fluid">
+        {block && (
+          <div className="row">
+            <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+              <div className="intro__heading">
+                {block.eyelet && <h4>{block.eyelet}</h4>}
+                {block.title && <h1>{block.title}</h1>}
+              </div>
+            </div>
+          </div>
+        )}
 
-				<div className="row">
-					<div className="col-12 d-flex flex-column align-items-center justify-content-center">
-						<div className="intro__data justify-content-center">
-							{startDate && (
-								<div>
-									<p className="--label">DATA APERTURA</p>
-									<p>{startDate}</p>
-								</div>
-							)}
-							{endDate && (
-								<div>
-									<p className="--label">DATA CHIUSURA</p>
-									<p>{endDate}</p>
-								</div>
-							)}
-						</div>
+        <div className="row">
+          <div className="col-12 d-flex flex-column align-items-center justify-content-center">
+            <div className="intro__data justify-content-center">
+              {startDate && (
+                <div>
+                  <p className="--label">DATA APERTURA</p>
+                  <p>{startDate}</p>
+                </div>
+              )}
+              {endDate && (
+                <div>
+                  <p className="--label">DATA CHIUSURA</p>
+                  <p>{endDate}</p>
+                </div>
+              )}
+            </div>
 
-						{hasPositionsData && (
-							<div className="intro__data justify-content-center">
-								{openPositions && (
-									<div>
-										<p className="--label">POSIZIONI RICERCATE</p>
-										<p>{openPositions}</p>
-									</div>
-								)}
-								{hiredPositions && (
-									<div>
-										<p className="--label">POSIZIONI ASSUNTE</p>
-										<p>{hiredPositions}</p>
-									</div>
-								)}
-							</div>
-						)}
+            {hasPositionsData && (
+              <div className="intro__data justify-content-center">
+                {openPositions && (
+                  <div>
+                    <p className="--label">POSIZIONI RICERCATE</p>
+                    <p>{openPositions}</p>
+                  </div>
+                )}
+                {hiredPositions && (
+                  <div>
+                    <p className="--label">POSIZIONI ASSUNTE</p>
+                    <p>{hiredPositions}</p>
+                  </div>
+                )}
+              </div>
+            )}
 
-						{hasSelectionData && (
-							<div className="intro__data justify-content-center --auto-w">
-								<div>
-									<p className="--label">PERSONE SELEZIONATE</p>
-									<p>{selectedPeople}</p>
-								</div>
-							</div>
-						)}
-					</div>
-				</div>
+            {hasSelectionData && (
+              <div className="intro__data justify-content-center --auto-w">
+                <div>
+                  <p className="--label">PERSONE SELEZIONATE</p>
+                  <p>{selectedPeople}</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
 
-				{block && (
-					<div className="row job__intro">
-						<div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-							<SharedBlock data={block} />
-						</div>
-					</div>
-				)}
-			</div>
-		</header>
-	);
+        {block?.body && (
+          <div className="row job__intro">
+            <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+              <SharedBlockBody data={block?.body} />
+            </div>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 };
--- src/components/SharedBlock/index.ts
+++
@@ -1,1 +0,0 @@
-export { SharedBlock } from './SharedBlock';
