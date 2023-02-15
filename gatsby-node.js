5640e02e915ab7171a6b37af589d9e0021a2fcb5
Michele Moio <michele.moio@protonmail.com>
2023-02-15 14:28:10 +0100

    chore: graphql SharedBlock union type

---
+++ gatsby-node.js
@@ -0,0 +1,7 @@
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  const typeDefs = `
+    union SHARED_BLOCKS = STRAPI__COMPONENT_SHARED_BLOCK_CONTENTS_LIST | STRAPI__COMPONENT_SHARED_BLOCK_INTRO | STRAPI__COMPONENT_SHARED_BLOCK_LIST_ATTACHMENTS | STRAPI__COMPONENT_SHARED_BLOCK_VISUAL_TEXT | STRAPI__COMPONENT_SHARED_BLOCK_ATTACHMENTS_GRID
+  `;
+  createTypes(typeDefs);
+};
