72fae9fbb3d4d6914e4f72787a0344842ce65093
Michele Moio <michele.moio@protonmail.com>
2023-02-16 10:06:53 +0100

    refactor: comments and better naming

--- src/pages/media/newsletter/index.tsx
+++ src/pages/media/newsletter/index.tsx
@@ -1,52 +1,47 @@
 import { graphql, Link } from 'gatsby';
-import { useTranslation } from 'gatsby-plugin-react-i18next/dist';
 import React from 'react';
 import { Layout } from '../../../partials/Layout';
 
+// This query is used to fetch all newsletters in the current language
+// It is used in the NewsletterList component
 export const query = graphql`
   query AllNewsletter($language: String!) {
     allStrapiNewsletter(filter: { locale: { eq: $language } }) {
       edges {
         node {
-          id
-          slug
-          bannerNewsletter
-          eyelet
-          title
-          url_path
-          body {
-            data {
-              body
-            }
-          }
+          ...Newsletter
         }
+      }
+    }
+  }
+  fragment Newsletter on STRAPI_NEWSLETTER {
+    id
+    slug
+    bannerNewsletter
+    eyelet
+    title
+    url_path
+    body {
+      data {
+        body
       }
     }
   }
 `;
 
-const Newsletter = ({ data }: { data: Queries.AllNewsletterQuery }) => {
-  const {
-    i18n: { language },
-  } = useTranslation();
-
-  return (
-    <Layout>
-      <ul>
-        {data.allStrapiNewsletter.edges.map(({ node: pressRelease }) => (
-          <Link
-            key={pressRelease?.id}
-            to={
-              `${process.env.API_URL}/media/newsletter-outer-space/${pressRelease?.slug}` ||
-              '/#'
-            }
-          >
-            <div className="p-4">{pressRelease.title}</div>
-          </Link>
-        ))}
-      </ul>
-    </Layout>
-  );
-};
+const Newsletter = ({ data }: { data: Queries.AllNewsletterQuery }) => (
+  <Layout>
+    <ul>
+      {data.allStrapiNewsletter.edges.map(({ node: newsletter }) => (
+        <Link
+          key={newsletter?.id}
+          to={`${process.env.API_URL}/media/newsletter-outer-space/${newsletter?.slug}`}
+        >
+          <div className="p-4">{newsletter.title}</div>
+        </Link>
+      ))}
+    </ul>
+  </Layout>
+);
 
 export default Newsletter;
