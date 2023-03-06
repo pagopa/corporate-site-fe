4c402492dbb234a2d26ad3843d92aca131f0863b
Michele Moio <michele.moio@protonmail.com>
2023-03-06 08:40:47 +0100

    refactor: ts & playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,35 +1,10 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 import * as dotenv from 'dotenv';
+import { config } from './e2e/config';
 
 dotenv.config({ path: __dirname + `/.env.development` });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
-  testDir: './e2e',
-  timeout: 30 * 1000,
-  expect: {
-    timeout: 5000,
-  },
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
-  use: {
-    baseURL: process.env.API_URL,
-    actionTimeout: 0,
-    trace: 'on-first-retry',
-    headless: true,
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        locale: 'it-IT',
-      },
-    },
-  ],
-});
+export default defineConfig(config.playwright);
