a2d1b09e06d57cca3b4a13ff8668af2d1b2d14c4
Vito Falco <vitofalco@gmail.com>
2023-05-26 17:10:56 +0200

    add GA

---
+++ static/js/script-ga.js
@@ -0,0 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-XNW0W43V93', {
+  anonymize_ip: true,
+  cookie_expires: 60 * 60 * 24 * 28 * 6,
+});
